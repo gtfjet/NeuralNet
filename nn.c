3f446be492f4d2a5e38a5f32b576ab90e890f70e
Dave <gtfjet@gmail.com>
2016-06-29 17:06:07 -0400

    breakpoint

--- nn.c
+++ nn.c
@@ -6,8 +6,51 @@ double sig(double u) {
 	return 1.0/(1.0 + exp(-u));	
 }
 
+
+/*  Helper to find index of element */
+int FindIndex(double* x, va_list ap) {
+  int index=0;
+  for(int i=0;i<ndims;i++) {
+    int j=va_arg(ap,int);
+    Assert((j>=1)&&(j<=dim),"Index out of bounds");
+    index=index*dim+(j-1);
+  }
+  return index;
+}
+
+/* Indexing to set/get elements */
+DYMOLA_STATIC Real RealElement(const RealArray a,...) {
+  SizeType index;
+  va_list ap;
+  va_start(ap,a);
+  index=FindIndex(a.ndims,a.dims,ap);
+  va_end(ap);
+  return a.data[index];
+}
+
+/* Calculate error Partials recursively */
+void get_partials(double *dedw) {
+	
+	for(int i=0; i<nNeurons; i++) {
+		dedw[i] = -x[i];
+		
+		
+		
+		
+		
+
+		for(int j=1; j<(nLayers-1); j++) {
+			dedw[i] *= w[s[j-1]*s[j]];
+			get_partials();
+		}				
+	}		
+}
+
+
+
+
 void main() {
-	int s[] = {1,3,1};  //define structure of neural net
+	int s[] = {2,4,3,2};  //define structure of neural net
 	double* w;  		//pointer to weights
 	double* x;  		//pointer to neuron data
 	double* b;			//pointer to biases	
@@ -17,10 +60,10 @@ void main() {
 	int nLayers  = sizeof(s)/sizeof(int);
 	int nInputs  = s[0];
 	int nOutputs = s[nLayers];
-	double u, y, total, err;
+	double u1, u2, y1, y2, total, err1, err2;
 	int p, q, r;
 
-	/* Get size of network */
+	/* Get Size of Network */
 	for(int i=0; i<nLayers; i++) {
 		nNeurons += s[i];
 		if(i>0) {
@@ -29,18 +72,20 @@ void main() {
 	}		
 	printf("%i,%i,%i\n",nLayers,nNeurons,nWeights); 
 	
-	/* Allocate memory */
+	/* Allocate Memory */
 	w = (double*) calloc(nWeights,sizeof(double));
 	x = (double*) calloc(nNeurons,sizeof(double));
-	b = (double*) calloc(nNeurons-s[0],sizeof(double));
+	b = (double*) calloc(nNeurons-nInputs,sizeof(double));
 	dedw = (double*) calloc(nWeights,sizeof(double));
 	
 	
 	/* Train the net */ 
-	for(int n=0; n<=1000; n++) {
-		/* Calculate input/output CURRENTLY ONE OF EACH */
-		u = (double) n/100;
-		y = sin(u);
+	for(int n=0; n<=100; n++) {
+		/* Calculate input/output CURRENTLY TWO OF EACH */
+		u1 = (double) n/100;
+		u2 = (double) n/10;
+		y1 = sin(u1);
+		y2 = cos(u1*u2);
 		
 		/* Calculate output using the net */
 		p = 0; //step thru w
@@ -48,41 +93,34 @@ void main() {
 		r = 0; //step thru b
 		for(int i=1; i<nLayers; i++) {
 			for(int j=0; j<s[i]; j++) {
-				total = b[r];
+				total = b[r];  //start with bias
 				r++;
 				for(int k=0; k<s[i-1]; k++) {
 					total += w[p]*x[q+k];
 					p++;
 				}
-				x[q+s[i-1]+j] = total;
+				x[q+s[i-1]+j] = tanh(total);
 			}
 			q += s[i-1];
 		}
 		
-		/* Calculate error */ 
-		err = pow(y-x[nNeurons-1],2);
-		err = sqrt(err);
-		printf("%f,%f,%f,%f\n",u,y,x[nNeurons-1],err);	
+		/* Calculate SQUARE Error CURRENTLY TWO OUTPUTS*/ 
+		err = (y1-x[nNeurons-2])*(y1-x[nNeurons-2])/2 + (y2-x[nNeurons-1])*(y2-x[nNeurons-1])/2;
+		printf("%f,%f,%f,%f\n",u1,y1,x[nNeurons-2],err);	
 		
-		/* Calculate error partials recursively */
-		for(int i=0; i<nWeights; i++) {
-			dedw[i] = -x[i%nInputs];
-			for(int j=1; j<(nLayers-1); j++) {
-				dedw[i] *= w[s[j-1]*s[j]];
-			}				
-		}		
+		get_partials();
 		
 		/* Update weights using one-step Newton's */
 		for(int i=0; i<nWeights; i++) {
 			//w[i] -= err/dedw[i];
 		}
-	
+	 
 	}
 	
 	
 	
 	
-	
+	/* Free Memory */
 	free(w);
 	free(x);
 	free(b);
